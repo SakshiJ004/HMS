a6ccb93f678c71de270392d019a28323fc759b6c
Sakshi <sakshij4869005@gmail.com>
2025-11-05 15:41:05 +0530

    change in server

--- server/server.js
+++ server/server.js
@@ -119,19 +119,11 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-    // origin: function (origin, callback) {
-    //     if (!origin) return callback(null, true);
-    //     if (allowedOrigins.indexOf(origin) !== -1) {
-    //         callback(null, true);
-    //     } else {
-    //         callback(new Error('CORS policy: This origin is not allowed.'));
-    //     }
-    // },
     origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
-            console.error('❌ Blocked by CORS:', origin);
             callback(new Error('CORS policy: This origin is not allowed.'));
         }
     },
