b40bab3d4da4d41ac7011212fe3119bc0491b368
Sakshi <sakshij4869005@gmail.com>
2026-01-06 14:03:48 +0530

    change path

--- client/template/src/feature-module/routes/router.link.tsx
+++ client/template/src/feature-module/routes/router.link.tsx
@@ -1,5 +1,5 @@
 // import AuthCallback from "../components/auth/callback/AuthCallback";
-import ProtectedRoute from "../../core/common/ProtectedRoute";
+import ProtectedRoute from "../../core/common/protectedRoute";
 import EmailVerificationBasic from "../components/auth/email-verification/emailVerificationBasic";
 import Error404 from "../components/auth/error-modules/error404";
 import Error500 from "../components/auth/error-modules/error500";
