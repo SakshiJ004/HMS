af979d23f34ce58a1374556b6b589579d9d97f0d
Sakshi <sakshij4869005@gmail.com>
2026-04-18 12:30:21 +0530

    add home page route

--- client/template/src/feature-module/routes/router.tsx
+++ client/template/src/feature-module/routes/router.tsx
@@ -1,18 +1,19 @@
 
-import { Navigate, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import { authRoutes, publicRoutes } from "./router.link";
 import AuthFeature from "../feathure-components/authFeature";
 import Feature from "../feathure-components/feature";
 import { all_routes } from "./all_routes";
 import AuthCallback from "../components/auth/callback/AuthCallback";
+import HomePage from "../components/pages/home/homePage";
 
 
 const ALLRoutes: React.FC = () => {
   return (
     <>
       <Routes>
-        {/* Default Redirect to Login Page */}
-        <Route path="/" element={<Navigate to={all_routes.homePage} />} />
+        {/* Default Redirect to Home Page */}
+        <Route path="/" element={<HomePage />} />
 
         <Route path={all_routes.authCallback} element={<AuthCallback />} />
 
