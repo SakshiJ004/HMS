aa912ced734eb19eb668c2db88211333c1317d07
SakshiJ004 <160335063+SakshiJ004@users.noreply.github.com>
2026-06-17 15:32:46 +0530

    Update Patient.js solved fetching list in add appoint

--- server/models/Patient.js
+++ server/models/Patient.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const User = require('../models/User');
-
+const Doctor = require('../models/Doctor');
 // ✅ PATIENT-SPECIFIC SCHEMA
 const patientSchema = new mongoose.Schema({
     primaryDoctor: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Doctor',
+        ref: 'doctor',
     },
     languageSpoken: [String],
 });
